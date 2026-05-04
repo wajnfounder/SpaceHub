b6d2595c7c854728d1e0cc1621ecf5165939f4f2
رزان المطيري <rzanalmtyry@rzans-Laptop.local>
2026-05-05 01:05:47 +0300

    favorites app

--- favorites/views.py
+++ favorites/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from urllib import request
 
-# Create your views here.
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import favorites
+from spaces.models import Space
+from .models import Favorite
+
+
+@login_required
+def toggle_favorite(request, space_pk):
+    space = get_object_or_404(Space, pk=space_pk)
+    fav, created = Favorite.objects.get_or_create(user=request.user, space=space)
+    if not created:
+        fav.delete()
+        messages.info(request, f'تمت إزالة "{space.title}" من المفضلة.')
+    else:
+        messages.success(request, f'تمت إضافة "{space.title}" إلى المفضلة.')
+    return redirect(request.META.get('HTTP_REFERER', 'spaces:list'))
+
+
+@login_required
+def my_favorites(request):
+    favorites = Favorite.objects.filter(user=request.user).select_related('space')
+    return render(request, 'favorites/list.html', {'favorites': favorites})
